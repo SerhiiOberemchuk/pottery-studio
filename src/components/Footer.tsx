b45aca70534c8d9656924fbfc2835f36c6f7f31e
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-03-05 20:12:18 +0100

    added header

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Footer({}: Props) {
+  return <footer className=" md:w-1">Footer</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function Header({}: Props) {
+  return <header className=" bg-cyan-500 w-full ">Header</header>;
+}
+
+export default Header;
