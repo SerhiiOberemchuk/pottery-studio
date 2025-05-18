12cbc107866e37d422b1f26d47dab7777db1b225
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-05-18 13:52:35 +0200

    new start without tailwind

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-module.exports = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
+export default nextConfig;
