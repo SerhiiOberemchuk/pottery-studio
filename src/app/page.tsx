b91b8f32dcd314d861db641edabb36e4f2619c22
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-05-18 16:32:34 +0200

    added global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
+          <li className="title_big">
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
