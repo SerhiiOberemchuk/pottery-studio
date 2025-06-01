6458bb7f7d7bf058972df1c17abba2eb179bb038
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-06-01 23:52:43 +0200

    fix 320

--- src/components/sections/home_page/HeroSection/HeroSection.tsx
+++ src/components/sections/home_page/HeroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./hero-section.module.css";
 import LinkPrimary from "@/components/ui/LinkPrimary/LinkPrimary";
+import clsx from "clsx";
 
 function HeroSection() {
   return (
@@ -11,16 +12,24 @@ function HeroSection() {
         </p>
         <div className={styles.hero_image_container}>
           <div>
-            <Image
-              width={158}
-              height={196}
-              src={"/hero-section/little-foto.webp"}
-              alt="hero_little_foto_default"
-              className={styles.little_foto}
-            />
-            <h2 className="subtitle_2 black ">
-              We are a Studio offering Pottery and Classes
-            </h2>
+            <div>
+              <Title visible="tablet" />
+            </div>
+            <div className={styles.litle_foto_container}>
+              <div>
+                <Image
+                  width={158}
+                  height={196}
+                  src={"/hero-section/little-foto.webp"}
+                  alt="hero_little_foto_default"
+                  className={styles.little_foto}
+                />
+                <h2 className="subtitle_2 black ">
+                  We are a Studio offering Pottery and Classes
+                </h2>
+              </div>
+              <TextWithLink visible="tablet" />
+            </div>
           </div>
           <Image
             src={"/hero-section/hero_image.webp"}
@@ -30,17 +39,45 @@ function HeroSection() {
             className={styles.big_foto}
           />
         </div>
-        <h1 className="title_big brown_dark">EXPLORE THE ANCIENT ART</h1>
-        <p className="body_text_btns brown_dark">
-          Unleash your creativity in our pottery studio, offering inspiring
-          classes and a curated shop.
-        </p>
-        <LinkPrimary href="#shop" className="">
-          shop
-        </LinkPrimary>
+        <Title visible="mobile" />
+        <TextWithLink visible="mobile" />
       </div>
     </section>
   );
 }
 
 export default HeroSection;
+function TextWithLink({ visible }: { visible: "mobile" | "tablet" }) {
+  return (
+    <div
+      className={clsx(
+        styles.text_with_link_container,
+        visible === "mobile"
+          ? styles.hiden_from_tablet
+          : styles.visible_from_tablet
+      )}
+    >
+      <p className="body_text_btns brown_dark">
+        Unleash your creativity in our pottery studio, offering inspiring
+        classes and a curated shop.
+      </p>
+      <LinkPrimary href="#shop" className="">
+        shop
+      </LinkPrimary>
+    </div>
+  );
+}
+function Title({ visible }: { visible: "mobile" | "tablet" }) {
+  return (
+    <h1
+      className={clsx(
+        "title_big brown_dark",
+        visible === "mobile"
+          ? styles.hiden_from_tablet
+          : styles.visible_from_tablet
+      )}
+    >
+      EXPLORE THE ANCIENT ART
+    </h1>
+  );
+}
